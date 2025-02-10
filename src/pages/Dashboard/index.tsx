c116ba08478374d6a41d99a218e0e836fe5661ab
yuri <yuri.barros.luz.familia@gmail.com>
2025-02-10 19:06:25 -0300

    feat: input and button invite

--- src/pages/Dashboard/index.tsx
+++ src/pages/Dashboard/index.tsx
@@ -5,6 +5,11 @@ import walkieTalkie from "../../assets/walkieTalkie.png"
 import uniform from "../../assets/uniform.png"
 import map from "../../assets/map.png"
 import discord from "../../assets/discord.png"
+import plus from "../../assets/plus.svg"
+import { Search } from "../../assets/search"
+import { UpArrow } from "../../assets/UpArrow"
+import { DownArrow } from "../../assets/DownArrow"
+import { X } from "../../assets/X"
 
 export function Dashboard() {
   return (
@@ -17,7 +22,57 @@ export function Dashboard() {
       </Element.Status>
       <Element.Content>
         <Element.Members>
-          
+          <Element.Header>
+            <div>
+              <Search />
+              <input type="text" placeholder="Pesquisar por nome ou ID"/>
+            </div>
+            <button> <img src={plus} />CONVIDAR</button>
+          </Element.Header>
+
+          <Element.Table>
+            <table>
+              <thead>
+                <tr>
+                  <th>Membro</th>
+                  <th>Cargo</th>
+                  <th>Status</th>
+                  <th>Meta</th>
+                  <th>Ações</th>
+                </tr>
+              </thead>
+              <tbody>
+                <tr>
+                  <td>Yuri Barros [5592]</td>
+                  <td>Líder</td>
+                  <td>Online</td>
+                  <td>140/400</td>
+                  <td><UpArrow /> <DownArrow /> <X /></td>
+                </tr>
+                <tr>
+                  <td>Diego Araujo [2424]</td>
+                  <td>Sub-líder</td>
+                  <td>Online</td>
+                  <td>140/400</td>
+                  <td><UpArrow /> <DownArrow /> <X /></td>
+                </tr>
+                <tr>
+                  <td>Luan Barros [1010]</td>
+                  <td>Gerente</td>
+                  <td>Online</td>
+                  <td>140/400</td>
+                  <td><UpArrow /> <DownArrow /> <X /></td>
+                </tr>
+                <tr>
+                  <td>Thiago Barros [1818]</td>
+                  <td>Membro</td>
+                  <td>Online</td>
+                  <td>140/400</td>
+                  <td><UpArrow /> <DownArrow /> <X /></td>
+                </tr>
+              </tbody>
+            </table>
+          </Element.Table>
         </Element.Members>
 
         <Element.Activity>
--- src/pages/Dashboard/styles.ts
+++ src/pages/Dashboard/styles.ts
@@ -26,11 +26,65 @@ export const Members = styled.div`
   display: flex;
   flex-direction: column;
   align-items: center;
-  gap: 0.8rem;
+  gap: 0.6rem;
   border-radius: 6px;
   border: 1px solid ${( props ) => props.theme['border']};
   background: radial-gradient(79.03% 79.03% at 50% 20.97%, rgba(255, 255, 255, 0.07) 0%, rgba(153, 153, 153, 0.02) 100%);
-  `
+`
+
+export const Header = styled.div`
+  display: flex;
+  width: 100%;
+  height: 3.8rem;
+  gap: .6rem;
+
+  > div {
+    position: relative;
+  }
+
+  > div > svg {
+    position: absolute;
+    left: 1rem;
+    top: 50%;
+    transform: translateY(-50%);
+  }
+
+  > div > input {
+    min-width: 43.9rem;
+    height: 3.8rem;
+    padding: 1.1rem auto .9rem 1.2rem;
+    align-items: flex-start;
+    border-radius: 4px;
+    border: 1px solid rgba(255, 255, 255, 0.06);
+    background: linear-gradient(90deg, #1C1C1C 0%, #242424 100%);
+  }
+  
+
+  > button {
+    gap: .6rem;
+    height: 3.8rem;
+    width: 100%;
+    display: flex;
+    align-items: center;
+    justify-content: center;
+    font-family: 'PuristaSemiBold', sans-serif;
+    font-size: 1.4rem;
+    gap: .4rem;
+    border-radius: 4px;
+    color: #FFF;
+    border: 1px solid #8237EC;
+    background: radial-gradient(138.56% 100% at 0% 51.25%, #6523C2 0%, #8237EC 59.77%, #6523C2 100%);
+
+    > svg {
+      width: 1.6rem;
+      height: 1.6rem;
+    }
+  }
+`
+
+export const Table = styled.div`
+
+`
 
 export const Activity = styled.div`
    flex: 1; 
